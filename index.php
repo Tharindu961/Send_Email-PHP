aec4236a826cab93ea18fea9752ad935b721fc15
Tharindu Bandara <37532030+Tharindu961@users.noreply.github.com>
2020-05-24 22:15:10 +0530

    mostly done the code

--- index.php
+++ index.php
@@ -1,3 +1,97 @@
+<?php
+	$error = '';
+	$name = '';
+	$email = '';
+	$subject = '';
+	$message = '';
+
+	function clean_text($string)
+{
+	$string = trim($string);
+	$string = stripslashes($string);
+	$string = htmlspecialchars($string);
+	return $string;
+}
+
+if(isset($_POST["submit"]))
+{
+	if(empty($_POST["name"]))
+	{
+		$error .= '<p><label class="text-danger">Please Enter your Name</label></p>';
+	}
+	else
+	{
+		$name = clean_text($_POST["name"]);
+		if(!preg_match("/^[a-zA-Z ]*$/",$name))
+		{
+			$error .= '<p><label class="text-danger">Only letters and white space allowed</label></p>';
+		}
+	}
+	if(empty($_POST["email"]))
+	{
+		$error .= '<p><label class="text-danger">Please Enter your Email</label></p>';
+	}
+	else
+	{
+		$email = clean_text($_POST["email"]);
+		if(!filter_var($email, FILTER_VALIDATE_EMAIL))
+		{
+			$error .= '<p><label class="text-danger">Invalid email format</label></p>';
+		}
+	}
+	if(empty($_POST["subject"]))
+	{
+		$error .= '<p><label class="text-danger">Subject is required</label></p>';
+	}
+	else
+	{
+		$subject = clean_text($_POST["subject"]);
+	}
+	if(empty($_POST["message"]))
+	{
+		$error .= '<p><label class="text-danger">Message is required</label></p>';
+	}
+	else
+	{
+		$message = clean_text($_POST["message"]);
+	}
+	if($error == '')
+	{
+		require 'class/class.phpmailer.php';
+		$mail = new PHPMailer;
+		$mail->IsSMTP();								
+		$mail->Host = 'smtpout.secureserver.net';		
+		$mail->Port = '80';								
+		$mail->SMTPAuth = true;							
+		$mail->Username = 'tharindujaye69@gmail.com';					
+		$mail->Password = 'Tharindu@9610';					
+		$mail->SMTPSecure = '';							
+		$mail->From = $_POST["email"];					
+		$mail->FromName = $_POST["name"];				
+		$mail->AddAddress('tharindubandara69@gmail.com', 'Name');		
+		$mail->AddCC($_POST["email"], $_POST["name"]);	
+		$mail->WordWrap = 50;							
+		$mail->IsHTML(true);											
+		$mail->Subject = $_POST["subject"];				
+		$mail->Body = $_POST["message"];				
+		if($mail->Send())								
+		{
+			$error = '<label class="text-success">Thank you for contacting us</label>';
+		}
+		else
+		{
+			$error = '<label class="text-danger">There is an Error</label>';
+		}
+		$name = '';
+		$email = '';
+		$subject = '';
+		$message = '';
+	}
+
+}
+
+?>
+
 <!DOCTYPE html>
 <html>
 	<head>
